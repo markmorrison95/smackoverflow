a872f15e8c9d48e2d43f79955cd3561f68a02f76
Mark Morrison <markmorrison058@gmail.com>
2020-03-03 15:25:30 +0000

    deleted admin controller

--- controller/mainTest.java
+++ controller/mainTest.java
@@ -1,8 +1,33 @@
 package controller;
 
-public class mainTest{
+import java.util.HashMap;
+import java.util.Map;
 
-    public static void main(String[] args) {
-        new Controller();
+public class mainTest {
+
+    
+        public static String pangramCheck(String s) {
+            s.toLowerCase();
+            Map<Character, Boolean> alpha = new HashMap<Character, Boolean>();
+            for(char ch = 'a';ch <= 'z'; ch++){
+                alpha.put(ch, false);
+            }
+            for(int i = 0; i < s.length(); i++){
+                Character ch = s.charAt(i);
+                if(alpha.containsKey(ch)){
+                    alpha.replace(ch, true);
+                }
+            }
+            for(Character ch = 'a';ch <= 'z'; ch++){
+                if(alpha.get(ch) == false){
+                    return "not pangram";
+                }
+            }
+            return "pangram";
+    
+        }
+    
+        public static void main(String[] args) {
+            System.out.println(pangramCheck("We promptly judged antique ivory buckles for the next prize"));
+        }
     }
-}
