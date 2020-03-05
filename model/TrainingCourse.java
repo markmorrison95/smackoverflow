b070410dff8bd18cad0b7d882b3a960b6bda0a33
KateHickey26 <56359559+KateHickey26@users.noreply.github.com>
2020-03-05 11:20:04 +0000

    Added comments

--- model/TrainingCourse.java
+++ model/TrainingCourse.java
@@ -16,27 +16,37 @@ public class TrainingCourse {
         listOfTeachers = new ArrayList<Teacher>();
     }
 
+    // gets course name
     public String getCourseName(){
         return courseName;
     }
+    
+    // gets subject name
     public String getSubjectName(){
         return subjectName;
     }
 
+    // set teacher variable
     public void setTeacher(Teacher teacher){
         this.teacherName = teacher;
     }
 
+    // gets teacher name
     public Teacher getTeacher(){
         return this.teacherName;
     }
 
+    // gets list of teachers
     public ArrayList<Teacher> getListOfTeachers(){
         return listOfTeachers;
     }
+    
+    // adds tecaher to a list of teachers
     public void addTeacherToCourse(Teacher teacher){
         listOfTeachers.add(teacher);
     }
+    
+    // to string method
     public String toString(){
         return courseName + " " + subjectName;
     }
